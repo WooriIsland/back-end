e25dc2a50519b01807ab6346b1a1dae77dbcbab3
jinvvoo <jinvvookim@gmail.com>
2023-11-06 11:22:41 +0900

    [Test] : Unity와 간단한 Get/POST 통신
    1. Get 통신 준비 완료
    - 테스트 성공 메시지 return
    
    2. Post 통신 준비 완료
    - Unity에서 Id 와 Password를 전달 받기.

--- src/main/java/com/blacky/our_island/controller/TestController.java
+++ src/main/java/com/blacky/our_island/controller/TestController.java
@@ -1,4 +1,40 @@
 package com.blacky.our_island.controller;
 
+import com.blacky.our_island.dto.TestDTO;
+import com.blacky.our_island.service.TestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
 public class TestController {
+
+    private final TestService testService;
+
+//    @Autowired
+//    public TestController(TestService testService) {
+//        this.testService = testService;
+//    }
+
+    @GetMapping("/user")
+    public String test() {
+
+        return "테스트 성공";
+    }
+
+    @PostMapping("/sign-up")
+    public String test2(@RequestBody TestDTO testDTO) {
+
+        System.out.println("요청 값" + testDTO);
+
+        testService.creatUser();
+
+        return "회원가입 완료";
+
+    }
+
 }
--- src/main/java/com/blacky/our_island/dto/TestDTO.java
+++ src/main/java/com/blacky/our_island/dto/TestDTO.java
@@ -1,4 +1,18 @@
 package com.blacky.our_island.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
 public class TestDTO {
+
+    private String Id;
+    private String password;
+
+
 }
