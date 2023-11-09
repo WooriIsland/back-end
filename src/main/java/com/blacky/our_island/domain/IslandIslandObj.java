ceac524c3f34ba05039e8971d93c0c751da885c1
jinvvoo <jinvvookim@gmail.com>
2023-11-09 17:44:54 +0900

    [BugFix] : [IslandIslandObj] 가족별 가족섬 오브젝트 엔티티 수정
    1. PUT 메소드 수정
    
    2. POST 메소드 수정
    
    3. IslandIslandObj 관련 코드 수정

--- src/main/java/com/blacky/our_island/domain/IslandIslandObj.java
+++ src/main/java/com/blacky/our_island/domain/IslandIslandObj.java
@@ -11,14 +11,14 @@ public class IslandIslandObj {
     @Column(name = "island_island_obj_id")
     private Long islandIslandObjId;
 
-    @Column(name = "pos_x", nullable = false)
-    private Double posX;
+    @Column(name = "position_x", nullable = false)
+    private Double positionX;
 
-    @Column(name = "pos_y", nullable = false)
-    private Double posY;
+    @Column(name = "position_y", nullable = false)
+    private Double positionY;
 
-    @Column(name = "pos_z", nullable = false)
-    private Double posZ;
+    @Column(name = "position_z", nullable = false)
+    private Double positionZ;
 
     @Column(name = "rotation_x", nullable = false)
     private Double rotationX;
@@ -38,7 +38,84 @@ public class IslandIslandObj {
     @Column(name = "island_obj_id", nullable = false)
     private Long islandObjId;
 
-    // Getters and Setters
+    public Long getIslandIslandObjId() {
+        return islandIslandObjId;
+    }
 
-    // Constructors
+    public void setIslandIslandObjId(Long islandIslandObjId) {
+        this.islandIslandObjId = islandIslandObjId;
+    }
+
+    public Double getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(Double positionX) {
+        this.positionX = positionX;
+    }
+
+    public Double getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(Double positionY) {
+        this.positionY = positionY;
+    }
+
+    public Double getPositionZ() {
+        return positionZ;
+    }
+
+    public void setPositionZ(Double positionZ) {
+        this.positionZ = positionZ;
+    }
+
+    public Double getRotationX() {
+        return rotationX;
+    }
+
+    public void setRotationX(Double rotationX) {
+        this.rotationX = rotationX;
+    }
+
+    public Double getRotationY() {
+        return rotationY;
+    }
+
+    public void setRotationY(Double rotationY) {
+        this.rotationY = rotationY;
+    }
+
+    public Double getRotationZ() {
+        return rotationZ;
+    }
+
+    public void setRotationZ(Double rotationZ) {
+        this.rotationZ = rotationZ;
+    }
+
+    public Double getRotationW() {
+        return rotationW;
+    }
+
+    public void setRotationW(Double rotationW) {
+        this.rotationW = rotationW;
+    }
+
+    public Long getIslandId() {
+        return islandId;
+    }
+
+    public void setIslandId(Long islandId) {
+        this.islandId = islandId;
+    }
+
+    public Long getIslandObjId() {
+        return islandObjId;
+    }
+
+    public void setIslandObjId(Long islandObjId) {
+        this.islandObjId = islandObjId;
+    }
+
 }
--- src/main/java/com/blacky/our_island/service/IslandIslandObjService.java
+++ src/main/java/com/blacky/our_island/service/IslandIslandObjService.java
@@ -5,7 +5,11 @@ import com.blacky.our_island.repository.IslandIslandObjRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class IslandIslandObjService {
@@ -13,28 +17,80 @@ public class IslandIslandObjService {
     @Autowired
     private IslandIslandObjRepository islandIslandObjRepository;
 
-    public List<IslandIslandObj> getAllIslandIslandObjs() {
-        return islandIslandObjRepository.findAll();
+    public List<Map<String, Object>> getAllIslandIslandObjs() {
+        return islandIslandObjRepository.findAll().stream()
+                .map(this::mapToResponseFormat)
+                .collect(Collectors.toList());
     }
 
-    public IslandIslandObj getIslandIslandObjById(Long islandIslandObjId) {
-        return islandIslandObjRepository.findById(islandIslandObjId).orElse(null);
+    public Map<String, Object> getIslandIslandObjById(Long islandIslandObjId) {
+        Optional<IslandIslandObj> islandIslandObjOptional = islandIslandObjRepository.findById(islandIslandObjId);
+        return islandIslandObjOptional.map(this::mapToResponseFormat).orElse(null);
     }
 
-    public IslandIslandObj createIslandIslandObj(IslandIslandObj islandIslandObj) {
-        return islandIslandObjRepository.save(islandIslandObj);
+    public void createIslandIslandObj(Map<String, Object> islandObjInfo) {
+        IslandIslandObj islandIslandObj = new IslandIslandObj();
+
+        islandIslandObj.setIslandId(Long.parseLong(islandObjInfo.get("island_id").toString()));
+        islandIslandObj.setIslandObjId(Long.parseLong(islandObjInfo.get("island_obj_id").toString()));
+
+        Map<String, Object> position = (Map<String, Object>) islandObjInfo.get("position");
+        islandIslandObj.setPositionX(Double.parseDouble(position.get("x").toString()));
+        islandIslandObj.setPositionY(Double.parseDouble(position.get("y").toString()));
+        islandIslandObj.setPositionZ(Double.parseDouble(position.get("z").toString()));
+
+        Map<String, Object> rotation = (Map<String, Object>) islandObjInfo.get("rotation");
+        islandIslandObj.setRotationX(Double.parseDouble(rotation.get("x").toString()));
+        islandIslandObj.setRotationY(Double.parseDouble(rotation.get("y").toString()));
+        islandIslandObj.setRotationZ(Double.parseDouble(rotation.get("z").toString()));
+        islandIslandObj.setRotationW(Double.parseDouble(rotation.get("w").toString()));
+
+        islandIslandObjRepository.save(islandIslandObj);
     }
 
-    public IslandIslandObj updateIslandIslandObj(Long islandIslandObjId, IslandIslandObj updatedIslandIslandObj) {
-        IslandIslandObj islandIslandObj = islandIslandObjRepository.findById(islandIslandObjId).orElse(null);
-        if (islandIslandObj != null) {
-            // Update fields as needed
-            return islandIslandObjRepository.save(islandIslandObj);
-        }
-        return null;
+    public void updateIslandIslandObj(Long islandIslandObjId, Map<String, Object> islandObjInfo) {
+        Optional<IslandIslandObj> islandIslandObjOptional = islandIslandObjRepository.findById(islandIslandObjId);
+        islandIslandObjOptional.ifPresent(obj -> {
+            obj.setIslandId(Long.parseLong(islandObjInfo.get("island_id").toString()));
+            obj.setIslandObjId(Long.parseLong(islandObjInfo.get("island_obj_id").toString()));
+
+            Map<String, Object> position = (Map<String, Object>) islandObjInfo.get("position");
+            obj.setPositionX(Double.parseDouble(position.get("x").toString()));
+            obj.setPositionY(Double.parseDouble(position.get("y").toString()));
+            obj.setPositionZ(Double.parseDouble(position.get("z").toString()));
+
+            Map<String, Object> rotation = (Map<String, Object>) islandObjInfo.get("rotation");
+            obj.setRotationX(Double.parseDouble(rotation.get("x").toString()));
+            obj.setRotationY(Double.parseDouble(rotation.get("y").toString()));
+            obj.setRotationZ(Double.parseDouble(rotation.get("z").toString()));
+            obj.setRotationW(Double.parseDouble(rotation.get("w").toString()));
+
+            islandIslandObjRepository.save(obj);
+        });
     }
 
     public void deleteIslandIslandObj(Long islandIslandObjId) {
         islandIslandObjRepository.deleteById(islandIslandObjId);
+    }
+
+    private Map<String, Object> mapToResponseFormat(IslandIslandObj islandIslandObj) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("island_id", islandIslandObj.getIslandId());
+        response.put("island_obj_id", islandIslandObj.getIslandObjId());
+
+        Map<String, Object> position = new HashMap<>();
+        position.put("x", islandIslandObj.getPositionX());
+        position.put("y", islandIslandObj.getPositionY());
+        position.put("z", islandIslandObj.getPositionZ());
+        response.put("position", position);
+
+        Map<String, Object> rotation = new HashMap<>();
+        rotation.put("x", islandIslandObj.getRotationX());
+        rotation.put("y", islandIslandObj.getRotationY());
+        rotation.put("z", islandIslandObj.getRotationZ());
+        rotation.put("w", islandIslandObj.getRotationW());
+        response.put("rotation", rotation);
+
+        return response;
     }
 }
